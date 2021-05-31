95d2965e099e9df6c43a08616fb20df5fcc52fac
Alex Colello <alexlcolello@gmail.com>
2021-05-31 15:35:00 -0700

    Added initialization script

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(realpath $SCRIPT_DIR)"
+cd $SCRIPT_DIR # Make sure working directory is directory of this script.
+
+sudo apt update
+sudo apt install cmake gcc-arm-none-eabi libnewlib-arm-none-eabi
+
+mkdir build
+cd build
+
+git clone https://github.com/raspberrypi/pico-sdk
+cd pico-sdk
+git submodule update --init
+cd ..
+
+cp pico-sdk/external/pico_sdk_import.cmake ..
+
+cmake .. -DPICO_SDK_PATH=./pico-sdk
+make
